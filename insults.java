9178fcccdf86055176c455238a7cdffcfd4ee066
phobo <80901567+phobologicalt@users.noreply.github.com>
2021-05-27 13:21:08 -0400

    Create insults.java

---
+++ insults.java
@@ -0,0 +1,14 @@
+public class insults{
+
+     public static void main(String []args){
+        System.out.println("lol you suck");
+        System.out.println("Your a dick");
+        System.out.println("Eat a bag of rocks shitface");
+        System.out.println("I hate you");
+        System.out.println("Your face is ugly");
+        System.out.println("Suck a fat one");
+        System.out.println("Your a dumbass");
+        System.out.println("Noone loves you");
+        System.out.println("Your a shitter");
+     }
+} 
