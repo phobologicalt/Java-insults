415774588dd208164add8bc06c5016be4a6aa1ae
phobo <80901567+phobologicalt@users.noreply.github.com>
2021-05-27 13:24:34 -0400

    Create Compliments.java

---
+++ Compliments.java
@@ -0,0 +1,11 @@
+public class compliments{
+
+     public static void main(String []args){
+         System.out.println("You look good today :^)");
+         System.out.println("You are a good person :^)");
+         System.out.println("Your mom loves you :^)");
+         System.out.println("I hope you have a great day :^)");
+         System.out.println("Your the nicest person I know :^)");
+         System.out.println("I hope you live a good life :^)");
+     }
+}
