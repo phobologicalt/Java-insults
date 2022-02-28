9a033928df7672b9f1aa1b6ea793f6b4038aa7bd
phobo <80901567+phobologicalt@users.noreply.github.com>
2022-02-28 13:44:11 -0500

    Create walterwhite.js

---
+++ walterwhite.js
@@ -0,0 +1,6 @@
+public class walterwhite{
+
+     public static void main(String []args){
+         System.out.println("My name is Walter Hartwell White. I live at 308 Negra Arroyo Lane Albuquerque New Mexico 87104. This is my confession.");
+     }
+} 
